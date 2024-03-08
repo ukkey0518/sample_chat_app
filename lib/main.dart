14dd856696dcebbca5d492e4b7f87d0b51d4f380
Ukkey <develop@ukihome.com>
2024-03-08 16:47:03 +0900

    Refactor main.dart to use ProviderScope and ProviderLogger for state management.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sample_chat_app/utils/provider_logger.dart';
 import 'package:sample_chat_app/view/app.dart';
 
 void main() {
-  runApp(const App());
+  runApp(
+    ProviderScope(
+      observers: [ProviderLogger()],
+      child: const App(),
+    ),
+  );
 }
