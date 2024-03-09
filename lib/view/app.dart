597d3ca354534c103c90090d2bd15fa9d1fdef3a
Ukkey <develop@ukihome.com>
2024-03-09 11:36:31 +0900

    Add light and dark theme to the MaterialApp in the App widget

--- lib/view/app.dart
+++ lib/view/app.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sample_chat_app/constants/theme.dart';
 import 'package:sample_chat_app/router/app_router.dart';
 
 class App extends ConsumerWidget {
   const App({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) => MaterialApp.router(
-        title: 'Sample Chat App',
-        routerConfig: ref.watch(goRouterProvider),
-      );
+  Widget build(BuildContext context, WidgetRef ref) {
+    return MaterialApp.router(
+      theme: appLightTheme,
+      darkTheme: appDarkTheme,
+      title: 'Sample Chat App',
+      routerConfig: ref.watch(goRouterProvider),
+    );
+  }
 }
