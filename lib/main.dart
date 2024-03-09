d5b59a031287bfafdd95cc3d14bfec469e84a300
Ukkey <develop@ukihome.com>
2024-03-09 11:10:16 +0900

    Updated firebase_core dependency to a direct main in pubspec.lock and added firebase_core ^2.26.0 in pubspec.yaml. Also initialized Firebase in main.dart.

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,39 @@
+import 'dart:async';
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sample_chat_app/constants/env.dart';
+import 'package:sample_chat_app/utils/debug_log.dart';
 import 'package:sample_chat_app/utils/provider_logger.dart';
 import 'package:sample_chat_app/view/app.dart';
 
 void main() {
-  runApp(
-    ProviderScope(
-      observers: [ProviderLogger()],
-      child: const App(),
-    ),
-  );
+  runZonedGuarded(() async {
+    WidgetsFlutterBinding.ensureInitialized();
+
+    // Initialize Firebase
+    await Firebase.initializeApp(
+      options: FirebaseOptions(
+        projectId: Env.firebaseProjectId,
+        apiKey: Env.firebaseApiKey,
+        appId: Env.firebaseAppId,
+        messagingSenderId: Env.firebaseMessagingSenderId,
+        authDomain: Env.firebaseAuthDomain,
+        storageBucket: Env.firebaseStorageBucket,
+        iosBundleId: Env.firebaseIosBundleId,
+      ),
+    );
+
+    // Run the app
+    runApp(
+      ProviderScope(
+        observers: [ProviderLogger()],
+        child: const App(),
+      ),
+    );
+  }, (error, stack) {
+    debugLog('runZonedGuarded: Caught error: $error');
+    debugLog(stack);
+  });
 }
