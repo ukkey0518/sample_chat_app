e13ab6dc77a389962a9464c8937a6f7ce8779907
Ukkey <develop@ukihome.com>
2024-03-10 16:05:28 +0900

    Refactor home_page.dart to include a SliverAppBar with a flexibleSpaceBar and a SliverList with dynamic content.

--- lib/view/screens/root/pages/home/home_page.dart
+++ lib/view/screens/root/pages/home/home_page.dart
@@ -5,8 +5,58 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('HomePage'),
+    return CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          pinned: true,
+          collapsedHeight: 80,
+          expandedHeight: 180,
+          backgroundColor: Colors.blue,
+          flexibleSpace: FlexibleSpaceBar(
+            titlePadding: const EdgeInsets.all(16),
+            title: Row(
+              children: [
+                const CircleAvatar(
+                  radius: 24,
+                  backgroundImage: NetworkImage(
+                    'https://avatars.githubusercontent.com/u/139426',
+                  ),
+                ),
+                const SizedBox(width: 12),
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Home',
+                      style: Theme.of(context).textTheme.titleMedium,
+                    ),
+                    Text(
+                      'Home',
+                      style: Theme.of(context).textTheme.labelSmall,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            background: const FlutterLogo(),
+          ),
+        ),
+        SliverList(
+          delegate: SliverChildBuilderDelegate(
+            (BuildContext context, int index) {
+              return Container(
+                color: index.isOdd ? Colors.white : Colors.black12,
+                height: 100,
+                child: Center(
+                  child: Text('$index', textScaler: const TextScaler.linear(5)),
+                ),
+              );
+            },
+            childCount: 20,
+          ),
+        ),
+      ],
     );
   }
 }
