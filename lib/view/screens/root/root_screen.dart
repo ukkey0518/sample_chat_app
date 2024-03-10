b68a1bd3c5062143d2c8a067565b75cd8a02f83b
Ukkey <develop@ukihome.com>
2024-03-10 11:50:32 +0900

    Refactor RootScreen to use Flutter Hooks for state management and implement a bottom navigation bar for switching between home, talk, and setting pages. Update enum class for RootPageType to define page types.

--- lib/view/screens/root/root_screen.dart
+++ lib/view/screens/root/root_screen.dart
@@ -1,19 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:sample_chat_app/view/screens/root/pages/home/home_page.dart';
+import 'package:sample_chat_app/view/screens/root/pages/setting/setting_page.dart';
+import 'package:sample_chat_app/view/screens/root/pages/talk/talk_page.dart';
 
-class RootScreen extends StatelessWidget {
-  const RootScreen({
-    super.key,
-  });
+class RootScreen extends HookWidget {
+  const RootScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final currentPageType = useState(RootPageType.home);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Sample Chat App'),
       ),
-      body: const Center(
-        child: Text('Root Screen!'),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentPageType.value.index,
+        onTap: (index) => currentPageType.value = RootPageType.values[index],
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat),
+            label: 'Talk',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Setting',
+          ),
+        ],
       ),
+      body: switch (currentPageType.value) {
+        RootPageType.home => const HomePage(),
+        RootPageType.talk => const TalkPage(),
+        RootPageType.setting => const SettingPage(),
+      },
     );
   }
 }
+
+/// An enum class that contains the types of
+/// pages that can be displayed in the root screen.
+enum RootPageType { home, talk, setting }
