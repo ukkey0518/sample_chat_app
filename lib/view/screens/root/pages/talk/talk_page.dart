520f3c77b62a70c2fabe60a5f3a28a1018d98735
Ukkey <develop@ukihome.com>
2024-03-10 11:40:49 +0900

    Added new pages (Home, Setting, Talk) and enum class (RootPageType) for root screen in the chat app.

---
+++ lib/view/screens/root/pages/talk/talk_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TalkPage extends StatelessWidget {
+  const TalkPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('TalkPage'),
+    );
+  }
+}
